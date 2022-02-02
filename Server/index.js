01ec3457000f0a78c726f5602c88b39f36cc7230
Guillermo Rodriguez <grodriguez@quantilogic.co>
2022-02-02 18:42:28 -0500

    addendums to UI and server functionality

--- Server/index.js
+++ Server/index.js
@@ -14,9 +14,13 @@ dispatch = new httpDispatch();
 // Router Function Definition
 function routerEngine(request, response){
     try{
-        console.log(request.url);
+        var occurred_on = new Date()
+        console.log("%s - %s", occurred_on.toString(), request.url);
+
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, content-type, origin, authorization, accept, client-security-token");
+        response.setHeader("Access-Control-Max-Age","1728000");
 
         dispatch.dispatch(request, response)
     }catch(err){
@@ -31,9 +35,61 @@ dispatch.onGet("/api/v1.0/software", function(request, response) {
     // Read Code File
     file.readFile('ui.js', 'utf8', function(err, contents){
         var encrypted_text = crypto.AES.encrypt(contents, KEY).toString();
-        console.log("File Contents Encrypted ...")
+
+        console.log("Application code encrypted!")
+
         response.end(encrypted_text);
     });
+});
+
+dispatch.onPost("/api/v1.0/account", function(request, response){
+    response.writeHead(200, {'Content-Type': 'application/json'});
+
+    var envelope = JSON.parse(request.body);
+    var outcome = false
+
+    console.log("%s:%s access requested", envelope.user, envelope.password);
+
+    try{
+      contents = file.readFileSync('account/account.data', 'utf8');
+      contents.split(/\r?\n/).forEach(line =>  {
+        account = line.split(',')
+
+        if (account.length == 3 && !outcome ) {
+            if( account[0] == envelope.user && account[1] == envelope.password && account[2] == 'valid'){
+              console.log("%s account validated", account[0])
+              outcome = true;
+            }
+        }
+      });
+
+    }catch(err){
+        response.end(JSON.stringify({ result: false, exception: err.message }));
+    }finally{
+      response.end(JSON.stringify({ result: outcome }));
+    }
+});
+
+dispatch.onPost("/api/v1.0/reservation", function(request, response){
+  response.writeHead(200, {'Content-Type': 'application/json'});
+
+  var envelope = JSON.parse(request.body);
+  var outcome = false
+
+  console.log("Reservation request: %s <%s>", envelope.name, envelope.email);
+
+  try{
+
+    data = envelope.date + ',' + envelope.time + "," + envelope.name + ',' + envelope.telephone + ',' + envelope.email + "\r\n";
+    file.appendFileSync("reservation/reservation.data", data);
+    outcome = true
+
+  }catch(err){
+      response.end(JSON.stringify({ result: false, exception: err.message }));
+  }finally{
+    response.end(JSON.stringify({ result: outcome }));
+  }
+
 });
 
 // Router Object Instantiation
--- Server/ui.js
+++ Server/ui.js
@@ -1,31 +1,62 @@
 
+var HOST = 'http://localhost:9595/api/v1.0/';
+var KEY = '1234567890';
+
 function create_row(label, input, caption){
 
-    var row = create_element('div', {'class': 'row', 'style': 'padding: 5px 5px 5px 5px; width: 800px; margin: auto; text-align: center;'});
-    $('body').append(row);
+    var row = create_element('div', {'class': 'row', 'style': 'padding: 5px 5px 5px 5px; width: 1000px; margin: auto; text-align: center;'});
+    $('#trs_container').append(row);
     var outer_div = null;
-    if( input == 'label' ){ outer_div = create_element('div', {'class': 'alert alert-primary', 'role': 'alert', 'id': input, 'style': 'width: 100%;'}, caption); }
-    else{ outer_div = create_element('div', {'class': 'input-group'}, ''); }
+    if( input == 'label' ){
+      if( label == 'ERROR' ){
+        outer_div = create_element('div', {'class': 'alert alert-danger', 'role': 'alert', 'style': 'width: 100%;'}, caption);
+      }else if( label == 'INFO'){
+        outer_div = create_element('div', {'class': 'alert alert-primary', 'role': 'alert', 'style': 'width: 100%;'}, caption);
+      }
+    }
+    else{
+      outer_div = create_element('div', {'class': 'input-group'}, '');
+    }
+
     $(row).append(outer_div);
-    if( label ){
+
+    if( label && (label != "ERROR" || label != "INFO") ){
         var inner_div = create_element('div', {'class': 'input-group-append'}, '');
         $(outer_div).append(inner_div);
         $(inner_div).append( create_element('span', {'class': 'input-group-text', 'style': 'width: 150px;'}, label) );
     }
 
-    if( input == 'text' ){
+    if( input == 'text' || input == 'password'){
+        // Text fields
         $(outer_div).append( create_element('input', {'class': 'form-control', 'type': input, 'id': label, 'value': caption}) );
 
     }else if( input == 'button' ) {
-         if (caption == 'Reserve'){
-            $(outer_div).append( create_element('button', {'class': 'btn btn-success', 'type': input, 'onclick': 'reserve()'}, caption) );
-        }else if (caption == 'Delete') {
-            $(outer_div).append( create_element('button', {'class': 'btn btn-success', 'type': input, 'onclick': 'delete()'}, caption) );
+        // Buttons
+        if (caption == 'Delete') {
+            $(outer_div).append( create_element('button', {'class': 'btn btn-danger', 'type': input, 'onclick': 'cancel_reservation()'}, caption) );
         }
-   }else if( input == 'link' ){
-       $(outer_div).append( create_element('a', {'class': 'btn btn-primary stretched-link', 'href': '#', 'onclick': 'return create_loan_inquiry()'}, caption));
-    }
+        else if (caption == 'Login') {
+              $(outer_div).append( create_element('button', {'class': 'btn btn-primary', 'type': input, 'onclick': 'login()'}, caption) );
+        }
+        else if (caption == 'Portal') {
+              $(outer_div).append( create_element('button', {'class': 'btn btn-primary', 'type': input, 'onclick': 'portal()'}, caption) );
+        }
+        else if (caption == 'Reserve'){
+          $(outer_div).append( create_element('button', {'class': 'btn btn-success', 'type': input, 'onclick': 'reserve()'}, caption) );
+        }
+    }else if( input == 'link' ){
+      // Links
+      $(outer_div).append( create_element('a', {'class': 'btn btn-primary stretched-link', 'href': '#', 'onclick': 'return create_loan_inquiry()'}, caption));
+   }
+}
 
+function clear_content(){
+    $('#trs_container').empty();
+}
+
+function create_container() {
+    var row = create_element('div', {'class': 'row', 'id': 'trs_container', 'style': 'padding: 5px 5px 5px 5px; width: 1000px; margin: auto; text-align: center;'});
+    $('body').append(row);
 }
 
 function create_element(object_type, attributes, innerText){
@@ -40,16 +71,86 @@ function create_element(object_type, attributes, innerText){
     return element;
 }
 
+function cancel_reservation() {
+  clear_content();
+
+
+  get_reservations();
+}
+
+function get_reservations() {
+  clear_content();
+
+}
+
+function login() {
+
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(request.responseText)
+        if( data.result ){
+          get_reservations();
+        }else{
+          create_row('Error', 'label', 'Invalid credentials provided.')
+        }
+      }
+  };
+
+  request.open("POST", HOST + 'account', true);
+  request.send(JSON.stringify({
+      user: $('#User').val(), password: $('#Password').val()
+    }));
+}
+
+function portal() {
+  clear_content();
+
+  // Layout
+  create_row('User', 'text', '');
+  create_row('Password', 'password', '');
+  create_row('', 'button', 'Login')
+}
+
+function reserve() {
+  clear_content();
+/*
+
+
+
+  request.send(JSON.stringify({
+      name: $('#Name').val(),
+      date: $('#Date').val(),
+      time: $('#Time').val(),
+      telephone: $('#Telephone').val(),
+      email: $('#Email').val()
+    }));
+    */
+}
+
+function set_hours() {
+
+
+}
+
 
 $( document ).ready( function() {
 
     var url = "http://trs:8888/book-your-table/";
 
     if (window.location.href == url ){
+      // Create base trs_container
+      create_container();
+
+      // Login section
+      create_row('', 'button', 'Portal');
+
+      // Default input
       create_row('Name', 'text', '');
       create_row('Date', 'text', '');
+      create_row('Time', 'text', '')
       create_row('Telephone', 'text', '');
-      create_row('email', 'text', '');
+      create_row('Email', 'text', '');
 
       create_row('', 'button', 'Reserve');
 
